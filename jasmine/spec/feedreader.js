09e4700781097abba3ab93b281a0add4a67b9ce8
Fergus Emmett <Fergus Emmett>
2016-11-20 10:09:24 -0600

    Made required and suggested changes as per feedback.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -10,9 +10,9 @@
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -31,89 +31,84 @@ $(function() {
          * in the allFeeds object and ensure it has a URL defined
          * and that the URL is not empty.
          */
-         it('URLs are defined', function() {
-           allFeeds.forEach(function(feed) {
-             expect(feed.url).toBeDefined();
-             expect(feed.url).not.toBe('');
-           });
-         });
+        it('URLs are defined', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            });
+        });
 
-         /* Loop through each feed
-          * in the allFeeds object and ensure it has a name defined
-          * and that the name is not empty.
-          */
-         it('names are defined', function() {
-           allFeeds.forEach(function(feed) {
-             expect(feed.name).toBeDefined();
-             expect(feed.name).not.toBe('');
-           });
-         });
+        /* Loop through each feed
+         * in the allFeeds object and ensure it has a name defined
+         * and that the name is not empty.
+         */
+        it('names are defined', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+        });
     });
 
     describe('The menu', function() {
 
-       /* Test that the menu element is hidden by default. */
-       it('is hidden by default', function() {
-         expect($('body').hasClass('menu-hidden')).toBe(true);
-       });
+        /* Test that the menu element is hidden by default. */
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
         /* Test that the menu changes visibility when the menu icon is clicked.*/
         it('changes visibility when icon clicked', function() {
-          var hasMenuHiddenClass = hasMenuHiddenClass = $('body').hasClass('menu-hidden');
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
 
-          $('.menu-icon-link').click();
-          expect($('body').hasClass('menu-hidden')).not.toBe(hasMenuHiddenClass);
-
-          $('.menu-icon-link').click();
-          expect($('body').hasClass('menu-hidden')).toBe(hasMenuHiddenClass);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
     });
 
     describe('Initial Entries', function() {
-      /*  Test that when the loadFeed
-       * function is called and completes its work, there is at least
-       * a single .entry element within the .feed container.
-       */
-       beforeEach(function(done) {
-         loadFeed(0, function() {
-           done();
-         });
-       });
+        /*  Test that when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
-       it('have at least one entry', function(done) {
-         var feedEntrySize = $('.feed .entry').size()
-         //console.log('feedEntrySize = ' + feedEntrySize);
-         expect(feedEntrySize).toBeGreaterThan(0);
-         done();
-       });
+        it('have at least one entry', function(done) {
+            var feedEntrySize = $('.feed .entry').size()
+            //console.log('feedEntrySize = ' + feedEntrySize);
+            expect(feedEntrySize).toBeGreaterThan(0);
+            done();
+        });
 
     });
 
     describe('New Feed Selection', function() {
-    /* Test that when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     */
-       var initialHTML, newHTML;
+        /* Test that when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         */
+        var initialHTML, newHTML;
 
-       beforeEach(function(done) {
-         loadFeed(0, function() {
-         // Load the first Feed and get the original/initial HTML
-           initialHTML = $('.feed').html();
-           //console.log('initialHTML = ' + initialHTML);
-           done();
-         });
-      });
+        beforeEach(function() {
+            loadFeed(0, function() {
+                // Load the first Feed and get the original/initial HTML
+                initialHTML = $('.feed').html();
+                //console.log('initialHTML = ' + initialHTML);
+            });
+        });
 
-      it('content actually changes', function(done) {
-         loadFeed(1, function () {
-           // Load the second Feed and get the HTML. Compare initial and new HTML
-           newHTML = $('.feed').html();
-           //console.log('newHTML = ' + newHTML);
-           expect(initialHTML).not.toBe(newHTML);
-           done();
-         });
-      });
+        it('content actually changes', function(done) {
+            loadFeed(1, function() {
+                // Load the second Feed and get the HTML. Compare initial and new HTML
+                newHTML = $('.feed').html();
+                //console.log('newHTML = ' + newHTML);
+                expect(initialHTML).not.toBe(newHTML);
+                done();
+            });
+        });
 
     });
 
